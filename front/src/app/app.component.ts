92fe58d43e6a35da30ed6c068de6de6733f9c1da
mojtaba <mojtaba.zare8131@gmail.com>
2024-06-18 09:09:17 -0400

    header added

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NbThemeModule } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    NbThemeModule,
+    RouterOutlet, 
+    HomeComponent, 
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- front/src/app/layout/header/header.component.ts
+++ front/src/app/layout/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NbIconConfig } from '@nebular/theme';
+import { NbThemeModule, NbTabsetModule, NbCardModule, NbStatusService } from '@nebular/theme';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [    
+    NbThemeModule,
+    NbTabsetModule,
+    NbCardModule
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  providers:[
+    NbStatusService
+  ]
 })
 export class HeaderComponent {
+  activeIndex: number = 0;
 
+  setActive(index: number) {
+    this.activeIndex = index;
+    console.log(this.activeIndex)
+  }
 }
