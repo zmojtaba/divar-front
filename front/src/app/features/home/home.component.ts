8bdea3ccc24ebc7f90c8f5de83ae46bcc1670e3b
PanizArdeshiri <phosseinpourardeshiri@gmail.com>
2024-07-12 13:07:38 -0700

    search and list pro added

--- front/src/app/features/home/home.component.ts
+++ front/src/app/features/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchComponent } from '../../layout/search/search.component';
-import { NbIconModule } from '@nebular/theme';
+import { NbIconModule, NbStatusService } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [SearchComponent,
+    NbEvaIconsModule, 
+    NbIconModule, 
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  providers: [NbStatusService]
 })
 export class HomeComponent implements OnInit {
   ngOnInit(): void {
--- front/src/app/layout/search/search.component.ts
+++ front/src/app/layout/search/search.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { NbCardModule, NbIconModule, NbStatusService, NbThemeModule } from '@nebular/theme';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+
 
 @Component({
   selector: 'app-search',
@@ -9,11 +13,21 @@ import { NbCardModule, NbIconModule, NbStatusService, NbThemeModule } from '@neb
   imports: [    
     NbEvaIconsModule, 
     NbIconModule, 
+    MatInputModule,
+    MatSelectModule
    ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss', 
   providers: [NbStatusService]
 })
+
+
+
 export class SearchComponent {
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
 
 }
