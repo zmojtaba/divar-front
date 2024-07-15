62aa29d7e946dc2b1d9adc52a2a981c91c004d70
PanizArdeshiri <phosseinpourardeshiri@gmail.com>
2024-07-15 07:32:25 -0700

    category added

--- front/src/app/features/category/category.component.ts
+++ front/src/app/features/category/category.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule, NbStatusService } from '@nebular/theme';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [    
+    CommonModule,
+    NbEvaIconsModule, 
+    NbIconModule,
+    MatExpansionModule ],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.scss'
+  styleUrl: './category.component.scss',
+  providers: [NbStatusService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryComponent {
+  readonly panelOpenState = signal(false);
+  realStateCats = ["apartment", "House", "Villa", "Studio", "Penthouse", "Residence", "Under Construction Building", "Land", "Commercial Property", "Warehouse"]
+  CarCats = ["Cabriolet", "coupe", "Hatchback 5 Door", "Hatchback 5 Door", "Sedan", "stationwagon", "SUV"]
+  OtherCats = ["Ashpaxkhane", "Electronic", "Animal"]
+
+  setCategory(cat_name:string){
+    console.log(cat_name)
+  }
 
 }
